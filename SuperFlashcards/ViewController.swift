d5e579f798c4949b469696031572ba1334ade08a
Joshua Manuel <josh@Joshuas-MacBook-Pro.local>
2018-03-31 19:19:58 -0400

    Initial commit; just added the initial layout and ViewControllers.

---
+++ SuperFlashcards/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SuperFlashcards
+//
+//  Created by Joshua Manuel on 3/31/18.
+//  Copyright © 2018 Joshua Manuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToVC(segue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
